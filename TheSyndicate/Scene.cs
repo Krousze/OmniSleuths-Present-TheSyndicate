90da7dad86a69e566aa7c604b6ebe0091c3e4f8a
Ruying <rychenf1@gmail.com>
2020-02-14 09:33:29 -0800

    game route

--- TheSyndicate/Scene.cs
+++ TheSyndicate/Scene.cs
@@ -88,7 +88,7 @@ namespace TheSyndicate
             TextBox dialogBox = new TextBox(this.Text, Program.WINDOW_WIDTH * 3 / 4, 2, (Program.WINDOW_WIDTH - (Program.WINDOW_WIDTH * 3 / 4)) / 2, 2);
             dialogBox.FormatText(this.Text);
             dialogBox.DrawDialogBox(this.Text);
-            tts.HearText(this.Text);
+            //tts.HearText(this.Text);
             //playVoice(); //??Asynchronous play
 
             //returning dialogBox for information about height of dialog box
